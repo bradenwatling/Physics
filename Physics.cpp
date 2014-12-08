fb9380b59544f93b22fc10435c0f5a1eb745ba70
Braden Watling <bradenwatling@hotmail.com>
2014-12-08 05:03:16 -0500

    Greatly improved collisions

--- Physics.cpp
+++ Physics.cpp
@@ -10,13 +10,12 @@ using namespace std;
 easygl window("Resistor display", WHITE);
 
 Physics::Physics() {
-    objects.push_back(new Rectangle(Point(0, -30), 100, 20, 100, true));
-    objects.push_back(new Rectangle(Point(12, 0), 3, 8, 100, false));
-    objects.push_back(new Rectangle(Point(-8, 0), 4, 8, 5, false));
+    objects.push_back(new Rectangle(Point(0, -30), 100, 20, 10, true));
+    objects.push_back(new Rectangle(Point(12, 0), 6, 8, 50, false));
+    objects.push_back(new Rectangle(Point(-8, -4), 4, 8, 5, false));
     
-    
-    objects[1]->applyForce(Point(-5, 5), Point(-40000, 180000));
-    objects[2]->applyForce(Point(-30, 5), Point(500, 8000));
+    objects[1]->applyForce(Point(-5, 5), Point(-40000, 180000) / 2.2);
+    objects[2]->applyForce(Point(30, 5), Point(500, 8000));
     
     window.set_world_coordinates(-100, -100, 100, 100);
 }
@@ -42,20 +41,27 @@ void Physics::run() {
     for (vector<Object *>::iterator i = objects.begin(); i != objects.end(); i++) {
         // Iterate over each unique pair
         for (vector<Object *>::iterator j = i + 1; j != objects.end(); j++) {
+            // If two Objects are colliding
             if (i != j && ((*i)->collision(**j) || (*j)->collision(**i))) {
-                // MoI1 * w1 + MoI2 * w2 = MoI1 * w1' + MoI2 * w2'
-                
+                // Calculate the new linear/angular velocities of each Object
                 Point iVel = (*i)->getVel(), jVel = (*j)->getVel();
                 
                 double iMass = (*i)->getMass(), jMass = (*j)->getMass();
                 double iAngVel = (*i)->getAngVel(), jAngVel = (*j)->getAngVel();
                 
-                double iMoment = iMass / (iMass + jMass) * (iAngVel + jAngVel);
-                double jMoment = jMass / (iMass + jMass) * (iAngVel + jAngVel);
+                double totalMass = iMass + jMass, totalAngVel = iAngVel + jAngVel;
                 
-                // Swap the linear velocities
-                (*i)->applyForce(iVel * -iMass + jVel * iMass, (iAngVel + iMoment) * -(*i)->getRoG());
-                (*j)->applyForce(jVel * -jMass + iVel * jMass, (jAngVel + jMoment) * -(*j)->getRoG());
+                double iMoment = iMass * totalAngVel / totalMass;
+                double jMoment = jMass * totalAngVel / totalMass;
+                
+                // Subtract 1 to cancel the velocity that each Object had previously
+                // Add the velocities, multiplied by the proportion of total mass going in the corresponding direction
+                Point iVelNew = iVel * (iMass / totalMass - 1) + jVel * (jMass / totalMass);
+                Point jVelNew = jVel * (jMass / totalMass - 1) + iVel * (iMass / totalMass);
+                
+                // Multiplying by mass/RoG because when we apply a force, we divide by mass/RoG
+                (*i)->applyForce(iVelNew * iMass, (iAngVel + iMoment) * -(*i)->getRoG());
+                (*j)->applyForce(jVelNew * jMass, (jAngVel + jMoment) * -(*j)->getRoG());
             }
         }
         
@@ -64,7 +70,6 @@ void Physics::run() {
     
     if (now - lastDraw > SPF) {
         draw();
-        
         window.gl_flush();
         
         lastDraw = now;
